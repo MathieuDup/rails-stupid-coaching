93b7b71d1a1a2143536e08c6071a2549ce3ed974
Mathieu Dupin <dupin.mathieu@gmail.com>
2022-05-16 12:22:52 +0200

    completed without css and test

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# coucou
+class AnswersController < ApplicationController
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# coucou
 class ApplicationController < ActionController::Base
 end
